dada0947620963e6aa4267a969d6377f0992e5a5
DanielWay <dway@ut.utm.edu>
2024-01-18 00:51:57 -0600

    Added asking the user for a number, n, and then sums the numbers from 1 to n, and returns the sum

--- main.cpp
+++ main.cpp
@@ -3,9 +3,33 @@
 
 using namespace std;
 
+
+int sum(int n);
+
+
 int main() {
 
+  // wasn't sure if I was supposed to leave this or not. So I kept it in
   cout << "Hello World!" << endl;
-  
+
+
+  int n;
+
+  cout << "Please enter a number ";
+  cin >> n;
+
+  int Answer = sum(n);
+  cout << "the sum is: " << Answer << endl;
+
   return 0;
+}
+
+int sum(int n) {
+  int result = 0;
+
+  for (int i = 1; i <= n; i++) {
+    result += i;
+  }
+
+  return result;
 }
